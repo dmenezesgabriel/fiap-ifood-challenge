3afe83141779decb196622aaf66958f1bea40d05
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2021-11-08 13:00:57 -0300

    Add Plan tests

---
+++ src/test/java/com/ifood/entity/PlanTest.java
@@ -0,0 +1,20 @@
+package com.ifood.entity;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class PlanTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Plan plan = new Plan();
+        plan.setName("Básico");
+        plan.setCommission(0.12);
+        plan.setTax(0.035);
+        plan.setPrice(100);
+        assertTrue(plan.getName() == "Básico");
+        assertTrue(plan.getCommission() == 0.12);
+        assertTrue(plan.getTax() == 0.035);
+        assertTrue(plan.getPrice() == 100);
+    }
+}
---
+++ src/test/java/com/ifood/entity/StoreTest.java
@@ -0,0 +1,5 @@
+package com.ifood.entity;
+
+public class StoreTest {
+
+}
